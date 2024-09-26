e6b3ceacf5c39e2aff3c61c6bc4d85c7eb421d4b
Leela Krishna <142929100+LeelaKrishna97@users.noreply.github.com>
2024-09-26 11:19:08 -0500

    Register page
    Updated Register Button and Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,7 @@ import AppContextProvider from "./store/AppContextProvider";
 import User from "./components/user/User";
 import Courses from "./components/courses/Courses";
 import Professors from "./components/professors/Professors";
+import Registerp from "./components/Registerp";
 
 function App() {
   const theme = createTheme(THEME);
@@ -27,6 +28,7 @@ function App() {
           <ToastContextProvider>
             <Routes>
               <Route path="/" element={<Login />} />
+              <Route path="/registerp" element={<Registerp />}></Route>
               <Route path="/user" element={<User />}>
                 <Route element={<Courses />} index={true}></Route>
                 <Route path="courses" element={<Courses />} />
