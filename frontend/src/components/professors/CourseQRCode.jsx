aeea1c20c00d39c424f14145c579753dd61a3ed6
SairakeshGajavalli <gajavallisairakesh1999@gmail.com>
2024-10-23 12:50:26 -0500

    update

--- frontend/src/components/professors/CourseQRCode.jsx
+++ frontend/src/components/professors/CourseQRCode.jsx
@@ -1,5 +1,7 @@
-import QRCode from "react-qr-code";
+//import QRCode from "react-qr-code";
 import { Modal, Title } from "@mantine/core";
+// Assuming you have a QR image in your project, import it like this:
+import qrImage from "./image.png"; // Adjust the path as needed
 
 const CourseQRCode = ({ open, setOpen, course }) => {
   return (
@@ -11,7 +13,18 @@ const CourseQRCode = ({ open, setOpen, course }) => {
       closeOnClickOutside={false}
     >
       <Title order={4}>{course.name || ""}</Title>
-      <QRCode value={`Successfully enrolled to the course! ${course.name}`} />
+
+      {/* You can either display the dynamically generated QR code */}
+      {/* <QRCode value={`Successfully enrolled to the course! ${course.name}`} /> */}
+
+      {/* OR display the local image */}
+      <img
+        src={qrImage}
+        alt="QR Code"
+        style={{ marginTop: "20px", width: "200px", height: "200px" }}
+      />
+
+
     </Modal>
   );
 };
