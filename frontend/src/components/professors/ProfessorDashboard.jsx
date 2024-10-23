b325551471200dd5971d30d687f2bd9697a11561
SairakeshGajavalli <gajavallisairakesh1999@gmail.com>
2024-10-23 13:20:12 -0500

    update

--- frontend/src/components/professors/ProfessorDashboard.jsx
+++ frontend/src/components/professors/ProfessorDashboard.jsx
@@ -1,10 +1,10 @@
 import { useState, useEffect } from "react";
-import { Flex, Box, ScrollArea, Title, Button, Modal } from "@mantine/core";
-import Cards from "./Cards"; // A component similar to Students' Cards
-import ProfessorDetails from "./ProfessorDetails"; // Component for displaying professor details
-import ProfessorForm from "./ProfessorForm"; // Form for adding/editing professors
-import { fetchProfessors, deleteProfessor } from "../../api/professorApi"; // API functions for professors
-import { fetchCourses } from "../../api/coursesApi"; // API functions for professors
+import { Flex, Box, Button } from "@mantine/core"; // Import Button here
+import Cards from "./Cards";
+import ProfessorDetails from "./ProfessorDetails";
+import ProfessorForm from "./ProfessorForm";
+import { fetchProfessors, deleteProfessor } from "../../api/professorApi";
+import { fetchCourses } from "../../api/coursesApi";
 import { useStore } from "../../store/useStore";
 
 const ProfessorDashboard = () => {
@@ -25,10 +25,20 @@ const ProfessorDashboard = () => {
   }, []);
 
   return (
-    <>
+    <Box>
       <ProfessorDetails data={professor} />
+      
+      {/* Add the "Students" button here */}
+      <Flex justify="flex-end" mt="md">
+        <Button
+          onClick={() => window.open("https://course-dashboard-jglh.onrender.com/display.html", "_blank")}
+        >
+          Students
+        </Button>
+      </Flex>
+
       <Cards courses={courses} />
-    </>
+    </Box>
   );
 };
 
